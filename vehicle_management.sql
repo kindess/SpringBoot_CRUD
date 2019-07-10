8137abc515a31089ca77f17ac18a1d962629d099
pengbinbin <616849824@qq.com>
2019-07-10 14:35:28 +0800

    增加权限、角色表相关表的sql

--- vehicle_management.sql
+++ vehicle_management.sql
@@ -16,6 +16,20 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`vehicle_management` /*!40100 DEFAULT CH
 
 USE `vehicle_management`;
 
+/*Table structure for table `authority_tb` */
+
+DROP TABLE IF EXISTS `authority_tb`;
+
+CREATE TABLE `authority_tb` (
+  `authority_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '权限主键',
+  `authority` varchar(50) NOT NULL COMMENT '角色(加了ROLE_前缀表示角色，没有表示权限)',
+  PRIMARY KEY (`authority_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+/*Data for the table `authority_tb` */
+
+insert  into `authority_tb`(`authority_id`,`authority`) values (1,'ROLE_ADMIN'),(2,'ROLE_USER'),(3,'selectVehicleInfo'),(4,'insertVehicleInfo'),(5,'deleteVehicleInfo'),(6,'updateVehicleInfo');
+
 /*Table structure for table `option_field_tb` */
 
 DROP TABLE IF EXISTS `option_field_tb`;
@@ -46,6 +60,19 @@ CREATE TABLE `option_tb` (
 
 insert  into `option_tb`(`option_id`,`option_name`,`option_field_name`) values (1,'车牌颜色','color'),(2,'车辆厂牌','vehicleBrand'),(3,'车辆类型','vehicleType'),(4,'车辆检修状态','vehicleMaintenanceStatus'),(5,'车辆年审状态','annualReviewStatus'),(6,'车辆燃料类型','fuelType'),(7,'等级','level'),(8,'业务用途','businessPurpose'),(9,'运营范围','scopeOfBusiness'),(10,'所属供应商','subordinateSuppliers'),(11,'调度权','dispatchingRight');
 
+/*Table structure for table `user_authority_tb` */
+
+DROP TABLE IF EXISTS `user_authority_tb`;
+
+CREATE TABLE `user_authority_tb` (
+  `user_id` varchar(22) NOT NULL COMMENT '用户主键',
+  `authority_id` int(11) NOT NULL COMMENT '权限主键'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `user_authority_tb` */
+
+insert  into `user_authority_tb`(`user_id`,`authority_id`) values ('111111111 ',1),('22222',2),('111111111 ',3),('111111111 ',4),('111111111 ',5),('111111111 ',6),('22222',3);
+
 /*Table structure for table `user_tb` */
 
 DROP TABLE IF EXISTS `user_tb`;
@@ -59,7 +86,7 @@ CREATE TABLE `user_tb` (
 
 /*Data for the table `user_tb` */
 
-insert  into `user_tb`(`id`,`username`,`password`) values ('22222','binbin','123456');
+insert  into `user_tb`(`id`,`username`,`password`) values ('111111111','城南花已开','123456'),('22222','binbin','123456');
 
 /*Table structure for table `vehicle_info_tb` */
 
@@ -99,7 +126,7 @@ CREATE TABLE `vehicle_info_tb` (
 
 /*Data for the table `vehicle_info_tb` */
 
-insert  into `vehicle_info_tb`(`id`,`vehicle_license`,`owener`,`option_color_id`,`engine_number`,`vehicle_brand_id`,`vehicle_rack_number`,`vehicle_model`,`register_date`,`vehicle_type_id`,`date_initial_registration`,`body_color`,`vehicle_maintenance_status_id`,`seating_capacity`,`annual_review_status_id`,`fuel_type_id`,`next_annual_inspection_time`,`displacement`,`level_id`,`business_purpose_id`,`scope_of_business_id`,`subordinate_suppliers_id`,`seats_of_guests`,`mileage`,`driving_license_number`,`dispatching_right_id`,`remarks`,`image_url`) values ('aNj2auu0h6WRYPZB4Hy46M','MLGB','啊啊啊啊啊',4,'111',7,'11111','1111','11111111',11,'1111111','111',13,1111,17,19,'11111',1111,20,22,24,29,11111,11,'1111111',30,'111111115555555','/upload/image/u=3361934473,3725527506&fm=26&gp=0.jpg'),('SpwPw1M2jl6ZasVxNDE3vg','川A65AR2','AHmeng',4,'123456',8,'520456','A车','2019-07-01 00:00:00',12,'2019-07-01 00:00:00','黄色',15,27,17,19,'2020-07-01 00:00:00',5000,21,22,24,29,26,250000,'5160021782941',30,'修改5555','/upload/image/u=3361934473,3725527506&fm=26&gp=0.jpg'),('V8szRi1viImpttZTIgrnJ0','233333','dddd',3,'ddddddd',8,'ddd','dd','dddddd',11,'ddddddd','dddd',15,22,17,18,'2222',22222,21,23,25,26,2222,22222,'222222',30,'AHmeng啊2333','/upload/image/u=3361934473,3725527506&fm=26&gp=0.jpg'),('_7wwAkx_g3K3HpsVfjURDM','QAQ123','pbb',1,'23232',8,'111','qq','2019-07-02 00:00:00',11,'2019-07-02 00:00:00','ggg',15,45,16,19,'2019-07-02 00:00:00',44444,20,22,24,27,34,65555555,'33333',31,'bilibili','/upload/image/u=3361934473,3725527506&fm=26&gp=0.jpg');
+insert  into `vehicle_info_tb`(`id`,`vehicle_license`,`owener`,`option_color_id`,`engine_number`,`vehicle_brand_id`,`vehicle_rack_number`,`vehicle_model`,`register_date`,`vehicle_type_id`,`date_initial_registration`,`body_color`,`vehicle_maintenance_status_id`,`seating_capacity`,`annual_review_status_id`,`fuel_type_id`,`next_annual_inspection_time`,`displacement`,`level_id`,`business_purpose_id`,`scope_of_business_id`,`subordinate_suppliers_id`,`seats_of_guests`,`mileage`,`driving_license_number`,`dispatching_right_id`,`remarks`,`image_url`) values ('aNj2auu0h6WRYPZB4Hy46M','MLGB','啊啊啊啊啊',4,'111',7,'11111','1111','2019-7-9',11,'2019-7-9','111',13,1111,17,19,'2019-7-9',1111,20,22,24,29,11111,11,'1111111',30,'111111115555555','/upload/image/QQ图片20190614110331.png'),('yVap8yzUgcWWpuyaVFFIWw','23333','按不出',3,'333333',5,'11111','3333333','2019-7-9',9,'2019-7-9','1',13,33333,17,18,'2019-7-9',3333,21,22,25,29,3333,3333,'1111111',31,'561651','/upload/image/u=1077360284,2857506492&fm=26&gp=0.jpg'),('YVw7FMs9jX6yN0vmZkZt9M','QAQ','pbb',1,'333333',8,'3333333333','3333333','2019-7-9',11,'2019-7-9','333333',13,33333,17,18,'2019-7-9',3333,20,23,24,26,33,3333,'333',31,'23333','/upload/image/u=1077360284,2857506492&fm=26&gp=0.jpg');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
